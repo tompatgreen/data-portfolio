0c684f57551417ede36f75e6b2ef0ee3dae82651
tom-greenuk <tompatgreen@hotmail.co.uk>
2026-03-22 23:16:40 +0000

    Delete Project 1 - Superstore directory

--- Project 1 - Superstore/Queries/01_total_sales.sql
+++
@@ -1,7 +0,0 @@
--- Query 1: Total sales
--- Business question: What is the total sales value?
--- Answer: 2297200.8602999472
--- Columns used: Sales
-SELECT
-  SUM(Sales) AS total_sales
-FROM `portfolio-490909.project1.superstore`
